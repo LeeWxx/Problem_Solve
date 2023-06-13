eb3f919e5203238ba69d7c8601e73173d8c7da0a
LeeWxx <uchang903@naver.com>
2023-06-13 13:20:56 +0000

    이전 순열

---
+++ Simulation/BOJ_10973.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int* arr, int N, bool result)
+{
+	if(!result)
+	{
+		cout << -1 << endl;
+		return;
+	}
+	
+	for(int i=0; i<N-1; i++)
+		cout << arr[i] << " ";
+	
+	cout << arr[N-1] << "\n";
+}
+
+bool next_permutaion(int* arr, int N)
+{
+	int first_idx(-1);
+	
+	for(int i=N-2; i>=0; i--)
+	{
+		if(arr[i] > arr[i+1])
+		{
+			first_idx = i;
+			break;
+		}
+	}
+	
+	if(first_idx == -1)
+		return false;
+	
+	for(int i=N-1; i>first_idx; i--)
+	{
+		if(arr[first_idx] > arr[i])
+		{
+			int temp = arr[i];
+			arr[i] = arr[first_idx];
+			arr[first_idx] = temp;
+			
+			break;
+		}
+	}
+	
+	int* temp = new int[N-(first_idx+1)];
+
+	for(int i=first_idx+1; i<N; i++)
+		temp[(N-1) - i] = arr[i];
+		
+	for(int i=first_idx+1; i<N; i++)
+		arr[i] = temp[i - (first_idx+1)];
+	
+	return true;
+}
+
+int main(int argc, char* argv[]) 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	
+	int* arr = new int[N];
+	
+	for(int i=0; i<N; i++)
+		cin >> arr[i];
+	
+	bool result = next_permutaion(arr, N);
+	
+	print(arr, N, result);
+}
